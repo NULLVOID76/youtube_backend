f2c41713266dceda45be98c67699e5fe17cbf444
NULLVOID76 <nullvoid76@gmail.com>
2024-07-29 08:17:21 +0530

    watch histroy pipeline and route update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -322,9 +323,67 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
   return res
     .status(200)
     .json(
-      new ApiResponse(200, channel[0], "User channel fetched successfully")
+      new ApiResponse(
+        200, 
+        channel[0], 
+        "User channel fetched successfully"
+      )
     );
 });
+
+const getUserWatchHistroy = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user?._id),
+      },
+      $lookup: {
+        from: "videos",
+        localField: "watchHistroy",
+        foreignField: "_id",
+        as: "watchHistroy",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistroy,
+        "watch histroy fetched successfully"
+      )
+    );
+});
+
+
 export {
   regitsterUser,
   loginUser,
@@ -336,4 +395,5 @@ export {
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
+  getUserWatchHistroy,
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,9 +4,17 @@ import {
   loginUser,
   logoutUser,
   refreshAcessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getUserWatchHistroy,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import multer from "multer";
 
 const userRouter = Router();
 
@@ -27,7 +35,14 @@ userRouter.route("/login").post(loginUser);
 
 //secured route
 userRouter.route("/logout").post(verifyJWT, logoutUser);
-userRouter.route("/refresh-token").post(refreshAcessToken)
+userRouter.route("/refresh-token").post(refreshAcessToken);
+userRouter.route("/change-password").post(verifyJWT, changeCurrentPassword);
+userRouter.route("/currect-user").get(verifyJWT, getCurrentUser);
+userRouter.route("/update-account").patch(verifyJWT, updateAccountDetails);
+userRouter.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+userRouter.route("/update-coverImage").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+userRouter.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+userRouter.route("/histroy").get(verifyJWT, getUserWatchHistroy);
 
 
 export default userRouter;
