8133c0ce2574b3ef17f419a5833e7be095698e8b
NULLVOID76 <nullvoid76@gmail.com>
2024-07-29 07:16:31 +0530

    pipeline in user channel

--- src/utils/clouninary.js
+++ src/utils/clouninary.js
@@ -1,31 +1,38 @@
-import {v2 as cloudinary} from "cloudinary"
-import fs from "fs"
-
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
 cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadOnCloudinary =async (localFilePath)=>{
-    try {
-        if(!localFilePath) return null;
-        const response=await cloudinary.uploader.upload(localFilePath,{
-            resource_type:"auto"
-        })
-        // file upload ho gyi
-        // console.log("file is uploaded on cloudinary",response.url);
-        console.log(`file response ${response}`,response);
-        return response;
-    } catch (error) {
-        console.log("file is not uploaded retry again");
-        return null
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file upload ho gyi
+    // console.log("file is uploaded on cloudinary",response.url);
+    console.log(`file response ${response}`, response);
+    return response;
+  } catch (error) {
+    console.log("file is not uploaded retry again");
+    return null;
+  } finally {
+    localFilePath && fs.unlinkSync(localFilePath);
+  }
+};
 
-    }
-    finally{
-        localFilePath && fs.unlinkSync(localFilePath)
-    }
+
+//complete it
+const deleteFromCloudniary = async(publicKey);
+{
+  try {
+    if (!publicKey) return null;
+    cloudinary.uploader.destroy(publicKey);
+
+  } catch (error) {}
 }
-
-export { uploadOnCloudinary}
+export { uploadOnCloudinary };
